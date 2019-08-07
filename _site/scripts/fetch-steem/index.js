a0bf167157ddca887c14adaece8e82f4ac86b0a2
Luke Stokes <luke.stokes@gmail.com>
2019-08-07 17:10:37 -0400

    Updating with the latest from master

--- _site/scripts/fetch-steem/index.js
+++ _site/scripts/fetch-steem/index.js
@@ -1,20 +1,15 @@
 const { Client } = require('dsteem');
 const client = new Client('https://api.steemit.com');
 
+
 if(!process.argv[2]) {
     console.error(process.argv[0], process.argv[1], 'PERMLINK [AUTHOR=eosdac]');
+    return;
 }
 
-var permlink = process.argv[2]
-var author = process.argv[3] || 'eosdac'
+let [author, permlink] = process.argv[2].split('/');
 
-var query = {
-    tag: '', // This tag is used to filter the results by a specific post tag.
-    limit: 5, // This allows us to limit the total to 5 items.
-    truncate_body: 1, // This will truncate the body of each post to 1 character, which is useful if you want to work with lighter array.
-};
-
-client.database.call('get_content', [author, permlink]).then(result => {
+client.database.call('get_content', [author.replace("@", ""), permlink]).then(result => {
     console.log('---');
     console.log('layout: post');
     console.log('title:  "'+result.title+'"');
